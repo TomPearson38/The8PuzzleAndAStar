277835f95627563b4a1faa8a5cce8db697f6959f
TomPearson38 <tomapearson38@gmail.com>
2022-04-14 17:40:19 +0100

    Added Successors Code

--- searchEngine/Astar/EpuzzleState.java
+++ searchEngine/Astar/EpuzzleState.java
@@ -23,4 +23,78 @@ public class EpuzzleState extends SearchState {
         }
     }
 
+    public ArrayList<SearchState> getSuccessors(Search searcher){
+        EpuzzleSearch Esearcher = (EpuzzleSearch) searcher;
+        int row;
+        int column;
+
+        for(int i = 0; i < board.count; i++){
+            for(int j =0; j < board[i].count; i++){
+                if(board[i][j] == 0){
+                    row = i;
+                    column = j;
+                }
+            }
+        }
+
+        ArrayList<EpuzzleState> puzList = new ArrayList<EpuzzleState>;
+        ArrayList<SearchState> statList = new ArrayList<SearchState>;
+
+
+        if(row == 0 && column == 0){
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,0,1)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,1,0)));
+        }
+        else if(row == 0 && column == 1){
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,0,0)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,0,2)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,1,1)));
+        }
+        else if(row == 0 && column == 2){
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,0,1)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,1,2)));
+        }
+        else if(row == 1 && column == 0){
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,0,0)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,1,1)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,2,0)));
+        }
+        else if(row == 1 && column == 1){
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,0,1)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,1,0)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,1,2)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,2,1)));
+        }
+        else if(row == 1 && column == 2){
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,0,2)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,1,1)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,2,2)));
+        }
+        else if(row == 2 && column == 0){
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,1,0)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,2,1)));
+        }
+        else if(row == 2 && column == 1){
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,1,1)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,2,0)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,2,2)));
+        }
+        else if(row == 2 && column == 2){
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,1,2)));
+            puzList.add(new EpuzzleState(moveBoardPiece(row,column,2,1)));
+        }
+
+    for (EpuzzleState puz : puzList){
+        statList.add((SearchState) puz);
+    }
+
+    }
+
+    private int[][] moveBoardPiece(int blankRow, int blankColumn, int valRow, int valColumn){
+        int[][] newBoard = board;
+        newBoard[blankRow][blankColumn] = newBoard[valRow][valColumn];
+        newBoard[valRow][valColumn] = 0;
+        return newBoard;
+    }
+
 }
