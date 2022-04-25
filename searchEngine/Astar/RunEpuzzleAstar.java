3c8a5226162dc5c6943d0c78483b55535f03f743
TomPearson38 <tomapearson38@gmail.com>
2022-04-25 16:55:41 +0100

    Changed how the code is ran Code now accomodates for both using pre-defined puzzles and randomly generated puzzles Can select which local cost algorithm to use (manhattan or hamming)

--- searchEngine/Astar/RunEpuzzleAstar.java
+++ searchEngine/Astar/RunEpuzzleAstar.java
@@ -10,30 +10,99 @@ import java.util.*;
 public class RunEpuzzleAstar{
 
     public static void main(String[] arg){
+      //User choses if they would like to chose predefined puzzles or would like to generate a new puzzle
+      Scanner myInput =new Scanner(System.in);  
+      System.out.println("Please enter the number of an option:");
+      System.out.println("[1] Pre-defined Puzzles");
+      System.out.println("[2] Random Puzzles");
+      int choice = myInput.nextInt();
+
+      if(choice == 1)
+        preDef();
+      else if (choice == 2)
+        randGen();
+      else
+        System.out.println("Invalid Input");
+    }
+
+    /**
+    *Solves a pre-defined puzzle
+    */
+    private static void preDef()
+    {
         int[][] startP1 = {{1, 0, 3}, {4, 2, 6}, {7, 5, 8}};
         int[][] startP2 = {{4, 1, 3}, {7, 2, 5}, {0, 8, 6}};
         int[][] startP3 = {{2, 3, 6}, {1, 5, 8}, {4, 7, 0}};
 
         int[][] target = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
 
-        //BFS search algorithm P1
+        System.out.println("Please chose a local cost algorithm:");
+        System.out.println("[1]Hamming");
+        System.out.println("[2]Manhattan");
+
+        Scanner myInput =new Scanner(System.in);  
+        int input = myInput.nextInt();
+        boolean hamming = true;
+
+        if(input == 2)
+          hamming = false;
+
+        //search algorithm P1 
         EpuzzleSearch searcherP1 = new EpuzzleSearch(target);
-        SearchState initStateP1 = (SearchState) new EpuzzleState(startP1, 100, false);
+        SearchState initStateP1 = (SearchState) new EpuzzleState(startP1, 100, hamming);
 
-        //BFS search algorithm P2
+        //search algorithm P2 
         EpuzzleSearch searcherP2 = new EpuzzleSearch(target);
-        SearchState initStateP2 = (SearchState) new EpuzzleState(startP2, 100, false);
+        SearchState initStateP2 = (SearchState) new EpuzzleState(startP2, 100, hamming);
 
-        //BFS search algorithm P3
+        //search algorithm P3 
         EpuzzleSearch searcherP3 = new EpuzzleSearch(target);
-        SearchState initStateP3 = (SearchState) new EpuzzleState(startP3, 100, false);
+        SearchState initStateP3 = (SearchState) new EpuzzleState(startP3, 100, hamming);
 
-        //Search algorithm is ran for each P
+        //algorithm is ran for each P 
         String resultP1 = searcherP1.runSearch(initStateP1, "AStar");
         System.out.println(resultP1);
         String resultP2 = searcherP2.runSearch(initStateP2, "AStar");
         System.out.println(resultP2);
         String resultP3 = searcherP3.runSearch(initStateP3, "AStar");
         System.out.println(resultP3);
+    }
+
+    /**
+    * Generates a random puzzle to solve based on a seed
+    */
+    private static void randGen(){
+        Scanner myInput = new Scanner( System.in );
+        System.out.print("Enter Seed: ");
+        int seed = myInput.nextInt();
+
+        //Puzzle is generated from presented seed
+        EpuzzGen generatePuzzle = new EpuzzGen(seed);
+        int[][] start = generatePuzzle.puzzGen(8);
+        int[][] goal = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+
+        //A* search algorithm using hamming search (boolean value is set to true)
+        EpuzzleSearch searcherHam = new EpuzzleSearch(goal);
+        SearchState initStateHam = (SearchState) new EpuzzleState(start, 100, true);
+
+        //A* search algorithm using manhattan search (boolean value is set to false)
+        EpuzzleSearch searcherManhat = new EpuzzleSearch(goal);
+        SearchState initStateManhat = (SearchState) new EpuzzleState(start, 100, false);
+
+        //Hamming method is ran 
+        System.out.println("Ham Method:");
+        String resdHam = searcherHam.runSearch(initStateHam, "AStar");
+
+        //Manhattan method is ran
+        System.out.println("\nManhattan Method:");
+        String resdManhat = searcherManhat.runSearch(initStateManhat, "AStar");
+        System.out.println("Starting map is: ");
+
+        //The board is printed into the console
+        String result = "";
+        for(int i = 0; i < start.length; i++){
+            for(int j =0; j < start[i].length; j++){
+        }
+      }
     }
 }
--- searchEngine/BFS/RunEpuzzleBFS.java
+++ searchEngine/BFS/RunEpuzzleBFS.java
@@ -1,7 +1,7 @@
 /**
   * RunEPuzzleBFS.java
   *
-  * run a BFS search for the 8 puzzle problem
+  * Run a BFS search for the 8 puzzle problem
   *
 */
 
@@ -10,6 +10,53 @@ import java.util.*;
 public class RunEpuzzleBFS{
 
     public static void main(String[] arg){
+      //User choses if they would like to chose predfined puzzles or would like to generate a random puzzle
+      Scanner myInput =new Scanner(System.in);  
+      System.out.println("Please enter the number of an option:");
+      System.out.println("[1] Pre-defined Puzzles");
+      System.out.println("[2] Random Puzzles");
+      int choice = myInput.nextInt();
+
+      if(choice == 1)
+        preDef();
+      else if (choice == 2)
+        randGen();
+      else
+        System.out.println("Invalid Input");
+    }
+
+    /**
+    * Generates a random puzzle to solve based on a seed
+    */
+    private static void randGen(){
+        //Puzzle is created
+        Scanner myInput = new Scanner( System.in );
+        System.out.print("Enter Seed (5 Digit Number): ");
+        int seed = myInput.nextInt();
+        EpuzzGen generatePuzzle = new EpuzzGen(seed);
+        int[][] start = generatePuzzle.puzzGen(8);
+        int[][] goal = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+
+        //Puzzle searcher is delcared
+        EpuzzleSearch searcher = new EpuzzleSearch(goal);
+        SearchState initState = (SearchState) new EpuzzleState(start);
+
+        //Puzzle is ran
+        String resd = searcher.runSearch(initState, "breadthFirst");
+        System.out.println(resd);
+
+        //The board is printed into the console
+        String result = "";
+        for(int i = 0; i < start.length; i++){
+            for(int j =0; j < start[i].length; j++){
+        }
+      }
+    }
+
+    /**
+    * Calculates the pre-defined puzzles presented
+    */
+    private static void preDef(){
         int[][] startP1 = {{1, 0, 3}, {4, 2, 6}, {7, 5, 8}};
         int[][] startP2 = {{4, 1, 3}, {7, 2, 5}, {0, 8, 6}};
         int[][] startP3 = {{2, 3, 6}, {1, 5, 8}, {4, 7, 0}};
